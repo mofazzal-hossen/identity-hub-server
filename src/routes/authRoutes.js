6821c2edbe3cdc61d998f65e3224a9a881e15266
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-31 00:21:35 +0600

    feat: add express-rate-limit dependency and update auth routes for user retrieval

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,84 +1,27 @@
 
-import { json, Router } from 'express';
+import { Router } from 'express';
 const routes = Router()
 
 
 /**
  * @openapi
- * /api/v1/auth/register:
- *   post:
- *     summary: Register a new user
- *     description: Creates a new user account using name, email, and password.
+ * /api/v1/auth/getall:
+ *   get:
+ *     summary: Get all users
  *     tags:
- *       - Authentication
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - name
- *               - email
- *               - password
- *             properties:
- *               name:
- *                 type: string
- *                 example: John Doe
- *               email:
- *                 type: string
- *                 format: email
- *                 example: john@example.com
- *               password:
- *                 type: string
- *                 format: password
- *                 example: John@123
+ *       - Auth
  *     responses:
- *       201:
- *         description: User registered successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                 message:
- *                   type: string
- *                   example: User registered successfully.
- *                 data:
- *                   type: object
- *                   properties:
- *                     _id:
- *                       type: string
- *                       example: 688a3d4e5f6a7b8c9d0e1234
- *                     name:
- *                       type: string
- *                       example: John Doe
- *                     email:
- *                       type: string
- *                       example: john@example.com
- *       400:
- *         description: Validation error or email already exists.
- *       500:
- *         description: Internal server error.
+ *       200:
+ *         description: Success
  */
-routes.post("/register", async (req, res)=>{
-    res.state(201).json({
-        successful:"true",
-        message:"user rej successfully"
-    })
+routes.get("/getall", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server Running",
+  });
 });
 
 
-
-
-// GET route
-// routes.get('/getall', (req, res) => {
-//     console.log("GET /getall hit");
-//     res.send("Server is dey");
-// });
 
 export default routes
 
