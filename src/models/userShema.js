74f936329d6dfe0688bfd84ad97d9feabb360893
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-31 18:38:07 +0600

    feat: create user schema and update auth routes for OTP handling

---
+++ src/models/userShema.js
@@ -0,0 +1,21 @@
+
+import mongoose from "mongoose";
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+
+    userName: {
+        type: String
+    },
+    otp: {
+        type: String,
+        default: null,
+    },
+})
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,24 +3,10 @@ import { Router } from 'express';
 const routes = Router()
 
 
-/**
- * @openapi
- * /api/v1/auth/getall:
- *   get:
- *     summary: Get all users
- *     tags:
- *       - Auth
- *     responses:
- *       200:
- *         description: Success
- */
-routes.get("/getall", (req, res) => {
-  res.json({
-    success: true,
-    message: "Server Running",
-  });
-});
-
+routes.post('/sendOpt',async(req, res)=>{
+  const {email}=req.body
+  
+})
 
 
 export default routes
